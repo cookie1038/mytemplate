7350abbab5790b6b13797c7688fdefc32951eb82
blinkfox <chenjiayin1990@163.com>
2016-02-06 22:59:49 +0800

    新增切换主题颜色的功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,21 @@
+/*设置主题时替换样式*/
+function replaceClass(color, c) {
+    $('.' + c + 'color').each(function() {
+        var newCls = '';
+        var cls =  $(this).attr('class').split(' ');
+        /*遍历去掉老的颜色值*/
+        for (var i = 0; i < cls.length; i++) {
+            var cl = cls[i];
+            if ((cl.indexOf(c) < 0) || ((c + 'color') == cl)) {
+                newCls = (i == cls.length - 1) ? newCls + cl : newCls + cl + ' ' ;
+            }
+        }
+        console.log('颜色样式：' + c + ',新样式：' + newCls);
+        $(this).attr('class', newCls);
+        $(this).addClass(c + color);
+    });
+}
+
 $(function() {
     /*菜单切换*/
     $('.button-collapse').sideNav();
@@ -11,6 +29,27 @@ $(function() {
     $('#search-mask .search-close').click(function() {
         $('#searchIcon').show();
         $('#search-mask').fadeOut(300);
+    });
+
+    /*调色面板显示开关*/
+    $('#theme-change .theme-change-handle').click(function() {
+        var l = $('#theme-change').attr('left');
+        if (l == -200) {
+            $('#theme-change').attr('left', 0).animate({
+                'left': '0'
+            });
+        } else {
+            $('#theme-change').attr('left', -200).animate({
+                'left': '-200px'
+            });
+        }
+    });
+    /*调色面板某颜色选中样式设置*/
+    $('#theme-change .theme-change-container ul li').click(function() {
+        $(this).addClass('selected').siblings().removeClass('selected');
+        var color = $(this).attr('data-colors');
+        replaceClass(color, 'bf-');
+        replaceClass(color, 'bf-text-');
     });
 
     /*回到顶部*/
