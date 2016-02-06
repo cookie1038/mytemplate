671aba2ae31dbb1647d6b44f2f10b05a665da853
blinkfox <chenjiayin1990@163.com>
2016-02-06 19:38:34 +0800

    新增回到顶部按钮及功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,4 +12,17 @@ $(function() {
         $('#searchIcon').show();
         $('#search-mask').fadeOut(300);
     });
+
+    /*回到顶部*/
+    $('.scrollSpy').scrollSpy();
+    /*回到顶部按钮根据滚动条的位置的显示和隐藏*/
+    $(window).scroll(function(event){
+        var t = $(window).scrollTop();
+        var ts = $('.top-scroll');
+        if (t < 50) {
+            ts.hide();
+        } else {
+            ts.show();
+        }
+    });
 });
